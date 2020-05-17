6e9cb58eab739e71bc7c457b3b9b34abc8d24137
mnaufalabdillah <naufal_abdillah@apps.ipb.ac.id>
2020-05-17 13:45:42 +0700

    Iseng Toast buat ambil detail

--- app/src/main/java/com/rpl6/foodmates/HomeActivity.java
+++ app/src/main/java/com/rpl6/foodmates/HomeActivity.java
@@ -4,17 +4,37 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     SessionManager sessionManager;
+    String nama;
+    private static String URL = "https://b3142241.ngrok.io/foodmates/userdetail.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,7 @@ public class HomeActivity extends AppCompatActivity {
 
         sessionManager = new SessionManager(this);
         sessionManager.checkLogin();
+        nama = sessionManager.getUserDetail().get("EMAIL");
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -43,9 +64,11 @@ public class HomeActivity extends AppCompatActivity {
                     break;
                 case R.id.nav_favorite:
                     selectedFragment = new FavoriteFragment();
+                    Toast.makeText(HomeActivity.this, nama, Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.nav_order:
                     selectedFragment = new OrderFragment();
+                    UserDetail(nama);
                     break;
                 case R.id.nav_profile:
                     selectedFragment = new ProfileFragment();
@@ -58,4 +81,48 @@ public class HomeActivity extends AppCompatActivity {
 
         }
     };
+
+    private void UserDetail(final String email) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("detail");
+
+                            if(success.equals("1")){
+                                for(int i=0; i<jsonArray.length(); i++){
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String email = object.getString("name").trim();
+
+                                    Toast.makeText(HomeActivity.this, email, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(HomeActivity.this, "Error "+e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(HomeActivity.this, "Error "+error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
 }
